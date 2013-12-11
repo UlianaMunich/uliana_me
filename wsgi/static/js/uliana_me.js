281baec1dee18f61e3553d669b9d6c478c326495
Uliana <like-me1@yandex.ru>
2013-12-11 18:25:57 +0100

    changed CV and added certicicates

--- wsgi/static/js/uliana_me.js
+++ wsgi/static/js/uliana_me.js
@@ -17,7 +17,7 @@ function initialize() {
         var map_canvas = document.getElementById('map_canvas');
         var map_options = {
           center: new google.maps.LatLng(51.04905654967599, 13.739862442016602),
-          zoom: 8,
+          zoom: 12,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map(map_canvas, map_options);
