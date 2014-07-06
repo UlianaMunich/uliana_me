8494718cce43f5713d3fc30f00dace678f202848
Uliana <like-me1@yandex.ru>
2014-07-06 21:52:27 +0200

    robots.txt add

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'views.index'),
     url(r'^career?/$', 'views.career'),
     url(r'^education?/$', 'views.education'),
@@ -13,4 +13,5 @@ urlpatterns = patterns('',
     url(r'^contact?/$', 'views.contact'),
     url(r'^api/contact$', 'views.send_mail'),
     url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt')),
+    )
